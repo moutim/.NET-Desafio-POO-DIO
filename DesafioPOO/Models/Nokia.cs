b1a16251ebfd1239a08077352636032e5837c3ab
Vitor Moutim <moutimg@gmail.com>
2024-01-10 03:37:20 -0300

    feat: herda classe Smartphone e implementa os metodos

--- DesafioPOO/Models/Nokia.cs
+++ DesafioPOO/Models/Nokia.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+        
+        public override void ApresentarAparelho(string modelo, string numero, string imei, int memoria)
+        {
+            Console.WriteLine($"Nokia {modelo} com {memoria}gb, número {numero} e IMEI {imei}.");
+        }
+
+        public override void InstalarAplicativo(string nomeApp, string modelo)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Nokia {modelo}.");
+        }
     }
 }
