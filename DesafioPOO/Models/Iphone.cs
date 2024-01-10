bd6cb82d6f53c2482807be1d36ce644384f36351
Vitor Moutim <moutimg@gmail.com>
2024-01-10 03:37:38 -0300

    feat: herda classe Smartphone e implementa os metodos na classe Iphone

--- DesafioPOO/Models/Iphone.cs
+++ DesafioPOO/Models/Iphone.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void ApresentarAparelho(string modelo, string numero, string imei, int memoria)
+        {
+            Console.WriteLine($"iPhone {modelo} com {memoria}gb, número {numero} e IMEI {imei}.");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp, string modelo)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no iPhone {modelo}.");
+        }
     }
 }
